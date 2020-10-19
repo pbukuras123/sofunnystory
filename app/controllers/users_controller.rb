49a2c915064caebec2edf53c0e30709e92d1b80a
Peter Bukuras <peterbukuras@gmail.com>
2020-10-18 21:53:34 -0400

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "Welcome to SoFunnyStory #{@user.username}, you have successfully signed up"
+			redirect_to stories_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+
 end
